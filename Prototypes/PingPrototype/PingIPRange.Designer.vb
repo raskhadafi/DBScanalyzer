58380381f1b9eeedb7c61b55fb9110b2b573c13a
Roman <roman.simecek@screenconcept.ch>
2010-03-05 15:41:14 +0100

    ip scanner can scan now an ip range

--- Prototypes/PingPrototype/PingIPRange.Designer.vb
+++ Prototypes/PingPrototype/PingIPRange.Designer.vb
@@ -23,42 +23,76 @@ Partial Class PingIPRange
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(PingIPRange))
-        Me.txtIPAdresse = New System.Windows.Forms.TextBox
+        Me.txtIPRangeStart = New System.Windows.Forms.TextBox
         Me.txtOutput = New System.Windows.Forms.TextBox
         Me.Button1 = New System.Windows.Forms.Button
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.txtIPRangeEnd = New System.Windows.Forms.TextBox
         Me.SuspendLayout()
         '
-        'txtIPAdresse
+        'txtIPRangeStart
         '
-        Me.txtIPAdresse.Location = New System.Drawing.Point(93, 12)
-        Me.txtIPAdresse.Name = "txtIPAdresse"
-        Me.txtIPAdresse.Size = New System.Drawing.Size(187, 20)
-        Me.txtIPAdresse.TabIndex = 5
-        Me.txtIPAdresse.Text = "10.0.2.15"
+        Me.txtIPRangeStart.Location = New System.Drawing.Point(93, 18)
+        Me.txtIPRangeStart.Name = "txtIPRangeStart"
+        Me.txtIPRangeStart.Size = New System.Drawing.Size(187, 20)
+        Me.txtIPRangeStart.TabIndex = 5
+        Me.txtIPRangeStart.Tag = ""
+        Me.txtIPRangeStart.Text = "10.0.2.15"
         '
         'txtOutput
         '
-        Me.txtOutput.Location = New System.Drawing.Point(12, 42)
+        Me.txtOutput.Location = New System.Drawing.Point(12, 87)
         Me.txtOutput.Multiline = True
         Me.txtOutput.Name = "txtOutput"
-        Me.txtOutput.Size = New System.Drawing.Size(268, 219)
+        Me.txtOutput.Size = New System.Drawing.Size(268, 259)
         Me.txtOutput.TabIndex = 4
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Location = New System.Drawing.Point(12, 18)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 3
         Me.Button1.Text = "ping"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(96, 4)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Start"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(96, 47)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(26, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "End"
+        '
+        'txtIPRangeEnd
+        '
+        Me.txtIPRangeEnd.Location = New System.Drawing.Point(93, 61)
+        Me.txtIPRangeEnd.Name = "txtIPRangeEnd"
+        Me.txtIPRangeEnd.Size = New System.Drawing.Size(187, 20)
+        Me.txtIPRangeEnd.TabIndex = 7
+        Me.txtIPRangeEnd.Tag = ""
+        Me.txtIPRangeEnd.Text = "10.0.2.15"
+        '
         'PingIPRange
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(292, 273)
-        Me.Controls.Add(Me.txtIPAdresse)
+        Me.ClientSize = New System.Drawing.Size(292, 358)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtIPRangeEnd)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtIPRangeStart)
         Me.Controls.Add(Me.txtOutput)
         Me.Controls.Add(Me.Button1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -68,7 +102,10 @@ Partial Class PingIPRange
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents txtIPAdresse As System.Windows.Forms.TextBox
+    Friend WithEvents txtIPRangeStart As System.Windows.Forms.TextBox
     Friend WithEvents txtOutput As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtIPRangeEnd As System.Windows.Forms.TextBox
 End Class
--- Prototypes/PingPrototype/PingIPRange.vb
+++ Prototypes/PingPrototype/PingIPRange.vb
@@ -5,15 +5,62 @@ Public Class PingIPRange
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         Dim ping As New NetworkInformation.Ping
         Dim pingResponse As NetworkInformation.PingReply
-        Dim ipAdresse As String
-        ipAdresse = txtIPAdresse.Text
-        pingResponse = ping.Send(ipAdresse, 50)
-        If pingResponse Is Nothing Then
-            txtOutput.Text += "No reply" + Environment.NewLine
-        ElseIf pingResponse.Status = IPStatus.Success Then
-            txtOutput.Text += "Reply from " + pingResponse.Address.ToString() + Environment.NewLine
-        Else
-            txtOutput.Text += "Ping was unsuccessful: " + pingResponse.Status.ToString() + Environment.NewLine
-        End If
+        'Dim ipRangeStart As String
+        'Dim ipRangeEnd As String
+        Dim ips() As String
+
+        ips = createIPs(txtIPRangeStart.Text, txtIPRangeEnd.Text)
+        'ipRangeEnd = txtIPRangeEnd.Text
+        For Each ip In ips
+            pingResponse = ping.Send(ip, 50)
+            If pingResponse Is Nothing Then
+                txtOutput.Text += "No reply" + Environment.NewLine
+            ElseIf pingResponse.Status = IPStatus.Success Then
+                txtOutput.Text += "Reply from " + pingResponse.Address.ToString() + Environment.NewLine
+            Else
+                txtOutput.Text += "Ping was unsuccessful: " + pingResponse.Status.ToString() + Environment.NewLine
+            End If
+        Next ip
     End Sub
+    ' creates array with ips from ip range
+    Private Function createIPs(ByVal startIP As String, ByVal endIP As String)
+        Dim datas(0) As String
+        Dim startIpArray(4) As String
+        Dim endIpArray(4) As String
+        Dim i As Integer
+
+        If startIP = endIP Then
+            datas(0) = startIP
+            Return datas
+        End If
+
+        i = 0
+        startIpArray = Split(startIP, ".")
+        endIpArray = Split(endIP, ".")
+
+        For first As Integer = startIpArray(0) To endIpArray(0)
+            For second As Integer = startIpArray(1) To endIpArray(1)
+                For third As Integer = startIpArray(2) To endIpArray(2)
+                    For fourth As Integer = startIpArray(3) To endIpArray(3)
+                        i += 1
+                    Next
+                Next
+            Next
+        Next
+        i = i - 1
+        ReDim datas(0 To i)
+        i = 0
+        For first As Integer = startIpArray(0) To endIpArray(0)
+            For second As Integer = startIpArray(1) To endIpArray(1)
+                For third As Integer = startIpArray(2) To endIpArray(2)
+                    For fourth As Integer = startIpArray(3) To endIpArray(3)
+                        datas(i) = first.ToString + "." + second.ToString + "." + third.ToString + "." + fourth.ToString
+                        i += 1
+                    Next
+                Next
+            Next
+        Next
+
+        Return datas
+    End Function
 End Class
