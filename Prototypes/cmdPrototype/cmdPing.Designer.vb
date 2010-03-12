de7de2f5b0cec73da39262f34ccb145cc36c82f7
Roman <roman.simecek@screenconcept.ch>
2010-03-12 13:25:41 +0100

    interaction with nmap.exe via cmd

---
+++ Prototypes/cmdPrototype/cmdPing.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class cmdPing
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.txtOutput = New System.Windows.Forms.TextBox
+        Me.txtStdout = New System.Windows.Forms.TextBox
+        Me.txtStderr = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(86, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "nmap.exe"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(140, 14)
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(414, 20)
+        Me.txtOutput.TabIndex = 1
+        '
+        'txtStdout
+        '
+        Me.txtStdout.Location = New System.Drawing.Point(12, 75)
+        Me.txtStdout.Multiline = True
+        Me.txtStdout.Name = "txtStdout"
+        Me.txtStdout.Size = New System.Drawing.Size(268, 188)
+        Me.txtStdout.TabIndex = 2
+        '
+        'txtStderr
+        '
+        Me.txtStderr.Location = New System.Drawing.Point(286, 75)
+        Me.txtStderr.Multiline = True
+        Me.txtStderr.Name = "txtStderr"
+        Me.txtStderr.Size = New System.Drawing.Size(268, 188)
+        Me.txtStderr.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(104, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "args:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Output:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(283, 59)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(67, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Error-Output:"
+        '
+        'cmdPing
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(569, 273)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtStderr)
+        Me.Controls.Add(Me.txtStdout)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "cmdPing"
+        Me.Text = "cmdPing"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txtOutput As System.Windows.Forms.TextBox
+    Friend WithEvents txtStdout As System.Windows.Forms.TextBox
+    Friend WithEvents txtStderr As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+End Class
---
+++ Prototypes/cmdPrototype/cmdPing.vb
@@ -0,0 +1,35 @@
+﻿Imports System.IO
+
+Public Class cmdPing
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        ' Set start information.
+        Dim start_info As New ProcessStartInfo("nmap.exe")
+        start_info.UseShellExecute = False
+        start_info.CreateNoWindow = True
+        start_info.RedirectStandardOutput = True
+        start_info.RedirectStandardError = True
+        start_info.Arguments = txtOutput.Text
+
+        ' Make the process and set its start information.
+        Dim proc As New Process()
+        proc.StartInfo = start_info
+
+        ' Start the process.
+        proc.Start()
+
+        ' Attach to stdout and stderr.
+        Dim std_out As StreamReader = proc.StandardOutput()
+        Dim std_err As StreamReader = proc.StandardError()
+
+        ' Display the results.
+        txtStdout.Text = std_out.ReadToEnd()
+        txtStderr.Text = std_err.ReadToEnd()
+
+        ' Clean up.
+        std_out.Close()
+        std_err.Close()
+        proc.Close()
+
+    End Sub
+End Class
