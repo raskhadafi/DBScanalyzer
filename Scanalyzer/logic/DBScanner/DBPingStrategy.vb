04c08c190b51345fc4b21e6d1c6f0275c3ec3783
Roman <roman.simecek@screenconcept.ch>
2010-03-26 19:34:27 +0100

    ping strategy inserted

--- Scanalyzer/logic/DBScanner/DBPingStrategy.vb
+++ Scanalyzer/logic/DBScanner/DBPingStrategy.vb
@@ -1,3 +1,6 @@
-﻿Public Class DBPingStrategy
+﻿Public MustInherit Class DBPingStrategy
+
+    Public MustOverride Function tryDefaultPort(ByRef ip As String) As ArrayList
+    Public MustOverride Function tryAllPorts(ByRef ip As String) As ArrayList
 
 End Class
