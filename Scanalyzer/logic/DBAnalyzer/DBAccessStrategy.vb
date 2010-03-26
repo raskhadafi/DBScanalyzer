afb393707e0a550f188e6f71be9d74ae74932892
Roman <roman.simecek@screenconcept.ch>
2010-03-26 19:31:31 +0100

    db access strategy insert

--- Scanalyzer/logic/DBAnalyzer/DBAccessStrategy.vb
+++ Scanalyzer/logic/DBAnalyzer/DBAccessStrategy.vb
@@ -1,3 +1,10 @@
-﻿Public Class DBAccessStrategy
+﻿Public MustInherit Class DBAccessStrategy
+
+    Public MustOverride Function openConnection(ByRef computer As Computer, ByVal databaseInstance As Integer) As Boolean
+    Public MustOverride Function closeConnection() As Boolean
+    Public MustOverride Function getInformationSchema() As ArrayList
+    Public MustOverride Function getDatabaseNames() As ArrayList
+    Public MustOverride Function getColumnNames() As ArrayList
+    Public MustOverride Function getColumn() As ArrayList
 
 End Class
