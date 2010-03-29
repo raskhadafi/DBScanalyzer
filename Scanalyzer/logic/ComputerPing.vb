0caccbd53fedb9e0d3a8486ac420585d96e1652a
Roman <roman.simecek@screenconcept.ch>
2010-03-29 20:03:52 +0200

    ping logic added

---
+++ Scanalyzer/logic/ComputerPing.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Net
+Imports System.Net.NetworkInformation
+
+Public Class ComputerPing
+
+    Public Function pingHost(ByRef ip As String)
+        Dim ping As New NetworkInformation.Ping
+        Dim pingResponse As NetworkInformation.PingReply
+        pingResponse = ping.Send(ip)
+        If pingResponse.Status = IPStatus.Success Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+End Class
