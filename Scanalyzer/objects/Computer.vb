ce6f7d2acfaa7b859eac0daaace75c8bc1b1b2b1
Roman <roman.simecek@screenconcept.ch>
2010-04-01 14:28:11 +0200

    getDatabaseInstance implemented

--- Scanalyzer/objects/Computer.vb
+++ Scanalyzer/objects/Computer.vb
@@ -3,4 +3,9 @@
     Public ip As String
     Public databasesInstances As ArrayList
 
+    Public Function getInstance(ByRef position As Integer) As DatabaseInstance
+        Return Me.databasesInstances(position)
+    End Function
+
+
 End Class
