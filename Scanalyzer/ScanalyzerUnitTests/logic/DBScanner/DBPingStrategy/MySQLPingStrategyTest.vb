2be1230f8276f4283310fb3c178b35c688e227d6
Roman <roman.simecek@screenconcept.ch>
2010-04-02 16:16:11 +0200

    start to implement mysql ping strategy

---
+++ Scanalyzer/ScanalyzerUnitTests/logic/DBScanner/DBPingStrategy/MySQLPingStrategyTest.vb
@@ -0,0 +1,90 @@
+﻿Imports System.Collections
+
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+Imports Scanalyzer
+
+
+
+'''<summary>
+'''This is a test class for MySQLPingStrategyTest and is intended
+'''to contain all MySQLPingStrategyTest Unit Tests
+'''</summary>
+<TestClass()> _
+Public Class MySQLPingStrategyTest
+
+
+    Private testContextInstance As TestContext
+
+    '''<summary>
+    '''Gets or sets the test context which provides
+    '''information about and functionality for the current test run.
+    '''</summary>
+    Public Property TestContext() As TestContext
+        Get
+            Return testContextInstance
+        End Get
+        Set(ByVal value As TestContext)
+            testContextInstance = Value
+        End Set
+    End Property
+
+#Region "Additional test attributes"
+    '
+    'You can use the following additional attributes as you write your tests:
+    '
+    'Use ClassInitialize to run code before running the first test in the class
+    '<ClassInitialize()>  _
+    'Public Shared Sub MyClassInitialize(ByVal testContext As TestContext)
+    'End Sub
+    '
+    'Use ClassCleanup to run code after all tests in a class have run
+    '<ClassCleanup()>  _
+    'Public Shared Sub MyClassCleanup()
+    'End Sub
+    '
+    'Use TestInitialize to run code before running each test
+    '<TestInitialize()>  _
+    'Public Sub MyTestInitialize()
+    'End Sub
+    '
+    'Use TestCleanup to run code after each test has run
+    '<TestCleanup()>  _
+    'Public Sub MyTestCleanup()
+    'End Sub
+    '
+#End Region
+
+
+    '''<summary>
+    '''A test for tryDefaultPort
+    '''</summary>
+    <TestMethod()> _
+    Public Sub tryDefaultPortTest()
+        Dim target As MySQLPingStrategy = New MySQLPingStrategy ' TODO: Initialize to an appropriate value
+        Dim ip As String = String.Empty ' TODO: Initialize to an appropriate value
+        Dim ipExpected As String = String.Empty ' TODO: Initialize to an appropriate value
+        Dim expected As ArrayList = Nothing ' TODO: Initialize to an appropriate value
+        Dim actual As ArrayList
+        actual = target.tryDefaultPort(ip)
+        Assert.AreEqual(ipExpected, ip)
+        Assert.AreEqual(expected, actual)
+        Assert.Inconclusive("Verify the correctness of this test method.")
+    End Sub
+
+    '''<summary>
+    '''A test for tryAllPorts
+    '''</summary>
+    <TestMethod()> _
+    Public Sub tryAllPortsTest()
+        Dim target As MySQLPingStrategy = New MySQLPingStrategy ' TODO: Initialize to an appropriate value
+        Dim ip As String = String.Empty ' TODO: Initialize to an appropriate value
+        Dim ipExpected As String = String.Empty ' TODO: Initialize to an appropriate value
+        Dim expected As ArrayList = Nothing ' TODO: Initialize to an appropriate value
+        Dim actual As ArrayList
+        actual = target.tryAllPorts(ip)
+        Assert.AreEqual(ipExpected, ip)
+        Assert.AreEqual(expected, actual)
+        Assert.Inconclusive("Verify the correctness of this test method.")
+    End Sub
+End Class
--- Scanalyzer/logic/DBScanner/DBPingStrategy/MySQLPingStrategy.vb
+++ Scanalyzer/logic/DBScanner/DBPingStrategy/MySQLPingStrategy.vb
@@ -1,3 +1,13 @@
 ﻿Public Class MySQLPingStrategy
+    Inherits DBPingStrategy
+
+
+    Public Overrides Function tryAllPorts(ByRef ip As String) As System.Collections.ArrayList
+        Return Nothing
+    End Function
+
+    Public Overrides Function tryDefaultPort(ByRef ip As String) As System.Collections.ArrayList
+        Return Nothing
+    End Function
 
 End Class
