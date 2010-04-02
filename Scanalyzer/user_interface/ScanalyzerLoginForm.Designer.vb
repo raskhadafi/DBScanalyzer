19844d6eb23a3361462c96b89438b1f29fae4038
Roman <roman.simecek@screenconcept.ch>
2010-04-02 16:14:17 +0200

    getCredentials Window designed

--- Scanalyzer/user_interface/ScanalyzerLoginForm.Designer.vb
+++ Scanalyzer/user_interface/ScanalyzerLoginForm.Designer.vb
@@ -23,17 +23,76 @@ Partial Class ScanalyzerLoginForm
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ScanalyzerLoginForm))
+        Me.txtUser = New System.Windows.Forms.TextBox
+        Me.lblUser = New System.Windows.Forms.Label
+        Me.lblPassword = New System.Windows.Forms.Label
+        Me.txtPassword = New System.Windows.Forms.TextBox
+        Me.btnSetCredentials = New System.Windows.Forms.Button
         Me.SuspendLayout()
+        '
+        'txtUser
+        '
+        Me.txtUser.Location = New System.Drawing.Point(90, 6)
+        Me.txtUser.Name = "txtUser"
+        Me.txtUser.Size = New System.Drawing.Size(190, 20)
+        Me.txtUser.TabIndex = 0
+        '
+        'lblUser
+        '
+        Me.lblUser.AutoSize = True
+        Me.lblUser.Location = New System.Drawing.Point(12, 9)
+        Me.lblUser.Name = "lblUser"
+        Me.lblUser.Size = New System.Drawing.Size(55, 13)
+        Me.lblUser.TabIndex = 1
+        Me.lblUser.Text = "Username"
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(12, 35)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(53, 13)
+        Me.lblPassword.TabIndex = 3
+        Me.lblPassword.Text = "Password"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(90, 32)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(190, 20)
+        Me.txtPassword.TabIndex = 2
+        Me.txtPassword.UseSystemPasswordChar = True
+        '
+        'btnSetCredentials
+        '
+        Me.btnSetCredentials.Location = New System.Drawing.Point(90, 58)
+        Me.btnSetCredentials.Name = "btnSetCredentials"
+        Me.btnSetCredentials.Size = New System.Drawing.Size(190, 23)
+        Me.btnSetCredentials.TabIndex = 4
+        Me.btnSetCredentials.Text = "set Credentials"
+        Me.btnSetCredentials.UseVisualStyleBackColor = True
         '
         'ScanalyzerLoginForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(292, 273)
+        Me.ClientSize = New System.Drawing.Size(292, 94)
+        Me.Controls.Add(Me.btnSetCredentials)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.lblUser)
+        Me.Controls.Add(Me.txtUser)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "ScanalyzerLoginForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "ScanalyzerLoginForm"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents txtUser As System.Windows.Forms.TextBox
+    Friend WithEvents lblUser As System.Windows.Forms.Label
+    Friend WithEvents lblPassword As System.Windows.Forms.Label
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents btnSetCredentials As System.Windows.Forms.Button
 End Class
--- Scanalyzer/user_interface/ScanalyzerLoginForm.vb
+++ Scanalyzer/user_interface/ScanalyzerLoginForm.vb
@@ -1,3 +1,10 @@
 ﻿Public Class ScanalyzerLoginForm
 
+    Private Sub ScanalyzerLoginForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnSetCredentials_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSetCredentials.Click
+
+    End Sub
 End Class
