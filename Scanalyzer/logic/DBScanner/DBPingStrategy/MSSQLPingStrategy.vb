ff6f3974923cf9fb98d914990e5af59f7fd2c253
Roman <roman.simecek@screenconcept.ch>
2010-04-08 15:59:44 +0200

    catch overflow exception in mssqlping strategy

--- Scanalyzer/logic/DBScanner/DBPingStrategy/MSSQLPingStrategy.vb
+++ Scanalyzer/logic/DBScanner/DBPingStrategy/MSSQLPingStrategy.vb
@@ -1,9 +1,32 @@
-﻿Public Class MSSQLPingStrategy
+﻿Imports System.Data.SqlClient
+
+Public Class MSSQLPingStrategy
     Inherits DBPingStrategy
 
+    Private connection As SqlConnection
 
     Public Overrides Function checkPorts(ByVal ip As String, ByVal ports As System.Collections.ArrayList) As System.Collections.ArrayList
-        Return Nothing
+        Dim connectionString As String
+        Dim mssqlPorts As ArrayList = New ArrayList
+
+        For Each i In ports
+            Dim portNumber As Integer = i
+            Dim answer As Boolean = False
+            connectionString = "Data Source=" + ip + "," + i.ToString + ";"
+            connection = New SqlConnection(connectionString)
+            Try
+                connection.Open()
+            Catch ex As TimeoutException
+
+            Catch ex As OverflowException
+
+            Catch ex As SqlException
+                If ex.Number = 18456 Then
+                    mssqlPorts.Add(i)
+                End If
+            End Try
+        Next
+        Return mssqlPorts
     End Function
 
 End Class
