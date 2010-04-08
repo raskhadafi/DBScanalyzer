1232d2e2dbd4af5bca3d961264dd7a9e4ba4cdb6
Roman <roman.simecek@screenconcept.ch>
2010-04-08 16:07:04 +0200

    dbaccessstrategy implemented

--- Scanalyzer/logic/DBAnalyzer/DBAccessStrategy/MSSQLAccessStrategy.vb
+++ Scanalyzer/logic/DBAnalyzer/DBAccessStrategy/MSSQLAccessStrategy.vb
@@ -1,3 +1,44 @@
-﻿Public Class MSSQLAccessStrategy
+﻿Imports System.Data.SqlClient
 
+Public Class MSSQLAccessStrategy
+    Inherits DBAccessStrategy
+
+    Private connection As SqlConnection
+    Private command As SqlCommand
+    Private connectionString As String
+
+    Public Overrides Function closeConnection() As Boolean
+
+        If connection.State = ConnectionState.Open Then
+            connection.Close()
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+
+    Public Overrides Function getColumn(ByVal databaseName As String, ByVal tableName As String, ByVal columName As String) As System.Collections.ArrayList
+        Return Nothing
+    End Function
+
+    Public Overrides Function getColumnNames(ByVal databaseName As String, ByVal tableName As String) As System.Collections.ArrayList
+        Return Nothing
+    End Function
+
+    Public Overrides Function getDatabaseNames() As System.Collections.ArrayList
+        Return Nothing
+    End Function
+
+    Public Overrides Function getInformationSchema() As System.Collections.ArrayList
+        Return Nothing
+    End Function
+
+    Public Overrides Function getTableNames(ByVal databaseName As String) As System.Collections.ArrayList
+        Return Nothing
+    End Function
+
+    Public Overrides Function openConnection(ByRef computer As Computer, ByVal databaseInstance As Integer) As Boolean
+        Return Nothing
+    End Function
 End Class
