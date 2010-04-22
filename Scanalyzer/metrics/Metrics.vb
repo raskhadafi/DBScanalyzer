209c2261248e2ef21b3041f4bb9482865c02de98
Jousi B. Marti <jousim@gmail.com>
2010-04-22 09:08:48 +0200

    metrics code started only trial

--- Scanalyzer/metrics/Metrics.vb
+++ Scanalyzer/metrics/Metrics.vb
@@ -1,3 +1,15 @@
 ﻿Public Class Metrics
 
+    ' Liste der Postleitzahlen
+    Dim plz As New List(Of String) ' TO DO: hier gilt es abzugklären, ob wir die PLZ als String oder als Integer auslesen
+
+    ' initialisiere plz
+    plz = 
+
+   Private Sub frm1202_Load( ... ) Handles MyBase.Load 
+        ' Initialisierung des Zufallsgenerators 
+        Randomize()
+        
+    End Sub
+
 End Class
