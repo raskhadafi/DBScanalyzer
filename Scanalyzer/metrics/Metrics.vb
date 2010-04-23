e0fd7e4d639207af056ac8245c7437a2421c13b7
Jousi B. Marti <jousim@gmail.com>
2010-04-23 14:52:58 +0200

    Update

--- Scanalyzer/metrics/Metrics.vb
+++ Scanalyzer/metrics/Metrics.vb
@@ -6,7 +6,18 @@
         Dim plz As New List(Of String) ' TO DO: hier gilt es abzugklären, ob wir die PLZ als String oder als Integer auslesen
 
         ' initialisiere plz
-        plz = 
+        REM plz = 
+
+        Public bool IsSameString(string referenceString, string dbString) ' compare databasestring with string in the referencefile
+        {
+        return (string.compare
+        }
+
+
+
+
+
+
 
         Private Sub frm1202_Load( ... ) Handles MyBase.Load 
 
