f25e2edb59ef858de3c540e9d54b5f26335bc3b8
Roman <roman.simecek@screenconcept.ch>
2010-04-23 16:11:33 +0200

    email recognition and gender recognition first draft implemented

--- Scanalyzer/metrics/Metrics.vb
+++ Scanalyzer/metrics/Metrics.vb
@@ -20,6 +20,67 @@ Namespace Metrics
 
         End Function
 
+        Public Function checkIfGender(ByVal data As ArrayList, ByVal references As ArrayList) As Integer
+
+            Dim entries As ArrayList = data
+            Dim propability As Integer = 0
+            Dim foundEntries As Integer = 0
+
+            distinctArray(entries)
+
+            ' When entries have a cardinality of 2 it's 20% propability of being a gender column
+            If entries.Count = 2 Then
+
+                propability = 20
+
+            End If
+
+            ' searches if references are in the entries
+            For Each ref In references
+
+                If entries.Contains(ref) Then
+
+                    foundEntries += 1
+
+                End If
+
+            Next
+
+            ' when entries which matches the references and the cardinality is 2
+            ' so it should be by 95% a gender column
+            If propability > 0 & foundEntries > 2 Then
+
+                propability = 95
+            Else
+
+                propability += foundEntries * 10
+
+            End If
+
+            Return propability
+
+        End Function
+
+        Private Sub distinctArray(ByRef data As ArrayList)
+
+            Dim distinctArray As ArrayList = New ArrayList
+
+            For Each entry In data
+
+
+                If Not distinctArray.Contains(entry) Then
+
+                    distinctArray.Add(entry)
+
+                End If
+
+
+            Next
+
+            data = distinctArray
+
+        End Sub
+
     End Class
 
 End Namespace
