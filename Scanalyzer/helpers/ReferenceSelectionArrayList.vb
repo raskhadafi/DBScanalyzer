75d8f3e74b387939b4155e5a56797246be00c58a
Roman <roman.simecek@screenconcept.ch>
2010-05-02 18:27:33 +0200

    helper array for the reference selection added

---
+++ Scanalyzer/helpers/ReferenceSelectionArrayList.vb
@@ -0,0 +1,58 @@
+﻿Namespace Helpers
+
+    Public Structure ReferenceSelection
+
+        Public reference As String
+        Public languages As ArrayList
+
+        Public Sub New(ByVal name As String)
+
+            reference = name
+            languages = New ArrayList
+
+        End Sub
+
+    End Structure
+
+    Public Class ReferenceSelectionArrayList
+        Inherits List(Of ReferenceSelection)
+
+        Public Function getReferenceSelection(ByVal term As String) As ReferenceSelection
+
+            Dim reference As ReferenceSelection
+
+            For Each entry In Me
+
+                If entry.reference.Contains(term) Then
+
+                    reference = entry
+
+                    Return reference
+
+                End If
+
+            Next
+
+            Return Nothing
+
+        End Function
+
+        Public Function checkIfContainsSelection(ByVal term As String) As Boolean
+
+            For Each entry In Me
+
+                If entry.reference.Contains(term) Then
+
+                    Return True
+
+                End If
+
+            Next
+
+            Return False
+
+        End Function
+
+    End Class
+
+End Namespace
