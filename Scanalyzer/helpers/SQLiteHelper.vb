795771c6bab95996f389ba5fa99654ff7c8d053b
Roman <roman.simecek@screenconcept.ch>
2010-05-02 14:43:02 +0200

    code refactored

--- Scanalyzer/helpers/SQLiteHelper.vb
+++ Scanalyzer/helpers/SQLiteHelper.vb
@@ -4,21 +4,47 @@ Namespace Helpers
 
     Public Module SQLiteHelper
 
+        Private connection As SQLiteConnection
+        Private sqlreader As SQLiteDataReader
+        Private cmd As SQLiteCommand
+
+        Public Sub getReferences(ByRef references As Array)
+
+
+
+        End Sub
+
+        Private Sub initializeSQLiteConnection()
+
+            connection = New SQLiteConnection()
+
+            connection.ConnectionString = "Data Source=C:\Dokumente und Einstellungen\Roman\Eigene Dateien\bda\DBScanalyzer\Scanalyzer\db\reference_data.sqlite;Version=3;New=False;Compress=True;"
+            ' TODO: sqlite should be generated
+            ' connection.ConnectionString = "Data Source=reference_data.sqlite;Version=3;New=False;Compress=True;"
+            connection.Open()
+
+        End Sub
+
+        Private Sub executeSQLCommand(ByVal sqlStatement As String)
+
+            cmd = connection.CreateCommand
+            cmd.CommandText = sqlStatement
+            sqlreader = cmd.ExecuteReader()
+
+        End Sub
+
+        Private Sub closeSQLiteConnection()
+
+            connection.Close()
+
+        End Sub
+
         Public Sub getReferenceData(ByVal tableName As String, ByRef entries As ArrayList)
 
             Try
 
-                Dim connection As New SQLiteConnection()
-                Dim sqlreader As SQLiteDataReader
-                Dim cmd As SQLiteCommand
-
-                connection.ConnectionString = "Data Source=C:\Dokumente und Einstellungen\Roman\Eigene Dateien\bda\DBScanalyzer\Scanalyzer\db\reference_data.sqlite;Version=3;New=False;Compress=True;"
-                ' TODO: sqlite should be generated
-                ' connection.ConnectionString = "Data Source=reference_data.sqlite;Version=3;New=False;Compress=True;"
-                connection.Open()
-                cmd = connection.CreateCommand
-                cmd.CommandText = "select value from " + tableName
-                sqlreader = cmd.ExecuteReader()
+                initializeSQLiteConnection()
+                executeSQLCommand("select value from " + tableName)
 
                 While sqlreader.Read
 
@@ -26,7 +52,7 @@ Namespace Helpers
 
                 End While
 
-                connection.Close()
+                closeSQLiteConnection()
 
             Catch ex As Exception
 
