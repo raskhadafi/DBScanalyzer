7f35e7977c20ab0517853ee7fa36f46ec66695f8
Roman <roman.simecek@screenconcept.ch>
2010-05-02 12:34:11 +0200

    sqlite helper added as a module

---
+++ Scanalyzer/helpers/SQLiteHelper.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Data.SQLite
+
+Namespace Helpers
+
+    Public Module SQLiteHelper
+
+        Public Sub getReferenceData(ByVal tableName As String, ByRef entries As ArrayList)
+
+            Try
+
+                Dim connection As New SQLiteConnection()
+                Dim sqlreader As SQLiteDataReader
+                Dim cmd As SQLiteCommand
+
+                connection.ConnectionString = "Data Source=|DataDirectory|\db\reference_data.sqlite;"
+                connection.Open()
+                cmd = connection.CreateCommand
+                cmd.CommandText = "select value from " + tableName
+                sqlreader = cmd.ExecuteReader()
+
+                While sqlreader.Read
+
+                    entries.Add(sqlreader(0))
+
+                End While
+
+                connection.Close()
+
+            Catch ex As Exception
+
+            End Try
+
+        End Sub
+
+    End Module
+
+End Namespace
