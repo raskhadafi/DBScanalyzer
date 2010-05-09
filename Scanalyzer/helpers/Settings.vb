7bf71bb26130bc681c70dc1929b8653b1861900b
Roman <roman.simecek@screenconcept.ch>
2010-05-09 13:30:23 +0200

    new helper object for the settings

---
+++ Scanalyzer/helpers/Settings.vb
@@ -0,0 +1,31 @@
+﻿Namespace Helpers
+
+    Public Class Settings
+
+        Private ips As List(Of String)
+
+        Public Sub New()
+
+            ips = New List(Of String)
+
+        End Sub
+
+        Public Sub addIP(ByVal ip As String)
+
+            If Not Me.ips.Contains(ip) Then
+
+                Me.ips.Add(ip)
+
+            End If
+
+        End Sub
+
+        Public Sub addIPRange(ByVal ipRange As String)
+
+
+
+        End Sub
+
+    End Class
+
+End Namespace
