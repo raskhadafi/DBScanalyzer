33bc4931b84e68e6bed0d7efa0bcaf34a84bc42c
Roman <roman.simecek@screenconcept.ch>
2010-05-15 16:04:23 +0200

    helper with helper functions added

---
+++ Scanalyzer/helpers/Helper.vb
@@ -0,0 +1,25 @@
+﻿Namespace Helpers
+
+    Public Module Helper
+
+        Public Function getDBAccessStrategy(ByVal type As Objects.DatabaseInstance.DatabaseEnum) As DBanalyzer.DBAccessStrategies.DBAccessStrategy
+
+            Dim access As DBanalyzer.DBAccessStrategies.DBAccessStrategy
+
+            access = Nothing
+
+            Select Case type
+
+                Case Objects.DatabaseInstance.DatabaseEnum.mysql
+
+                    access = New DBanalyzer.DBAccessStrategies.MySQLAccessStrategy
+
+            End Select
+
+            Return access
+
+        End Function
+
+    End Module
+
+End Namespace
