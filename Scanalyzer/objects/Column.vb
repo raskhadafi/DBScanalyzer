0c673354eb6461368e50bb566c3cec2f7f092e39
Roman <roman.simecek@screenconcept.ch>
2010-05-24 10:38:36 +0200

    new object table added

---
+++ Scanalyzer/objects/Column.vb
@@ -0,0 +1,23 @@
+﻿Namespace Objects
+
+    Public Class Column
+
+        Private name As String
+        Private containsReferencedata As Boolean
+
+        Public Sub New(ByVal name As String)
+
+            Me.name = name
+            Me.containsReferencedata = False
+
+        End Sub
+
+        Public Function getName() As String
+
+            Return Me.name
+
+        End Function
+
+    End Class
+
+End Namespace
