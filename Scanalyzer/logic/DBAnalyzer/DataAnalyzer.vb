17c0a73539a8ae4d22c9b504afefa4e7664380d7
Roman <roman.simecek@screenconcept.ch>
2010-05-25 20:22:43 +0200

    data analyzer prepared for report calculation

--- Scanalyzer/logic/DBAnalyzer/DataAnalyzer.vb
+++ Scanalyzer/logic/DBAnalyzer/DataAnalyzer.vb
@@ -52,11 +52,38 @@
 
         End Sub
 
+        Public Sub calculateReport()
+
+            For Each computer In Me.computers
+
+                For Each databaseInstance In computer.getDatabaseInstances
+
+                    For Each database In databaseInstance.getDatabases
+
+                        For Each table In database.getTables
+
+                            For Each column In table.getColumns
+
+
+
+                            Next
+
+                        Next
+
+                    Next
+
+                Next
+
+            Next
+
+        End Sub
+
         Private Sub analyze(ByVal computer As Objects.Computer, ByVal databaseInstance As Objects.DatabaseInstance, ByRef database As Objects.Database, ByRef table As Objects.Table, ByRef column As Objects.Column, ByVal tableCount As Integer)
 
             Dim access As DBAccessStrategies.DBAccessStrategy
             Dim entries As List(Of String) = New List(Of String)
             Dim entriesFromDB As ArrayList
+            Dim totalToAnalyze As Integer
             Dim value As Boolean = Nothing
             Dim total As Integer = 0
             Dim totalFound As Integer = 0
@@ -65,10 +92,22 @@
             Dim checkIfGenderTotal As Integer = 0
             Dim checkIfStreetTotal As Integer = 0
 
+
+            If Me.settings.analyzeEverthing Then
+
+                totalToAnalyze = tableCount
+
+            Else
+
+                totalToAnalyze = Me.settings.getDataAnalyzationLimit
+
+            End If
+
+
             access = Helpers.Helper.getDBAccessStrategy(databaseInstance.getDatabaseType)
             access.openConnection(computer, databaseInstance)
 
-            For i As Integer = 0 To tableCount Step 100
+            For i As Integer = 0 To totalToAnalyze Step 100
                 entriesFromDB = access.getColumnLimited(database.getName, table.getName, column.getName, i, (i + 99))
                 For Each entry In entriesFromDB
 
@@ -115,46 +154,35 @@
                 Next
 
                 access.closeConnection()
-
-                If checkIfDateTotal > checkIfEmailTotal & checkIfDateTotal > checkIfGenderTotal & checkIfDateTotal > checkIfEmailTotal Then
-
-                    totalFound = checkIfDateTotal
-
-                ElseIf checkIfEmailTotal > checkIfDateTotal & checkIfEmailTotal > checkIfGenderTotal & checkIfEmailTotal > checkIfStreetTotal Then
-
-                    totalFound = checkIfEmailTotal
-
-                ElseIf checkIfGenderTotal > checkIfDateTotal & checkIfGenderTotal > checkIfEmailTotal & checkIfGenderTotal > checkIfStreetTotal Then
-
-                    totalFound = checkIfGenderTotal
-
-                ElseIf checkIfStreetTotal > checkIfDateTotal & checkIfStreetTotal > checkIfEmailTotal & checkIfStreetTotal > checkIfGenderTotal Then
-
-                    totalFound = checkIfStreetTotal
-
-                Else
-
-                    totalFound = 0
-
-                End If
-
-                If checkIfEmailTotal > 0 Then
-
-                    totalFound = checkIfEmailTotal
-
-                End If
-
-                If total > 0 & totalFound > 0 Then
-
-                    totalFound = totalFound / total
-
-                End If
-
-                column.setFound(totalFound)
+                column.setFound(getTotalOfFoundMetrics(checkIfDateTotal, checkIfEmailTotal, checkIfGenderTotal, checkIfStreetTotal))
 
             Next
 
         End Sub
+
+        Private Function getTotalOfFoundMetrics(ByVal checkIfDateTotal As Integer, ByVal checkIfEmailTotal As Integer, ByVal checkIfGenderTotal As Integer, ByVal checkIfStreetTotal As Integer) As Integer
+
+            If checkIfEmailTotal > 0 Then
+
+                Return checkIfEmailTotal
+
+            ElseIf checkIfDateTotal > 0 Then
+
+                Return checkIfDateTotal
+
+            ElseIf checkIfGenderTotal > 0 Then
+
+                Return checkIfGenderTotal
+
+            ElseIf checkIfStreetTotal > 0 Then
+
+                Return checkIfStreetTotal
+
+            End If
+
+            Return 0
+
+        End Function
 
     End Class
 
